5cd6dfba7a0d0362d8486816035c97a2b9ae8aff
Matheus Fidelis <msfidelis01@gmail.com>
2018-05-03 22:59:07 -0300

    routes

--- main.go
+++ main.go
@@ -8,9 +8,15 @@ var addr = iris.Addr("0.0.0.0:3000")
 
 func main() {
 	app := iris.New()
-	app.Get("/healthcheck", index)
+	app.Get("/healthcheck", healthcheck)
+	app.Post("/gmud", gmud)
+	app.Run(addr)
 }
 
 func healthcheck(ctx iris.Context) {
-	ctx.JSON(iris.Map{"health": "status"})
+	ctx.JSON(iris.Map{"status": 200})
+}
+
+func gmud(ctx iris.Context) {
+	ctx.JSON(iris.Map{"feature": "create GMUD PDF"})
 }
